e55a266caa424c66d5dcd82cd83ba19de2208f75
andrewseybold <andrewseybold@gmail.com>
2019-07-17 14:22:02 -0600

    Updated Javadoc in Chassis

--- TeamCode/src/main/java/edu/ahs/robotics/Chassis.java
+++ TeamCode/src/main/java/edu/ahs/robotics/Chassis.java
@@ -2,6 +2,9 @@ package edu.ahs.robotics;
 
 import org.firstinspires.ftc.robotcore.internal.android.dx.util.Warning;
 
+/**
+ * THe Chassis class is the superclass for all chassis types
+ */
 public abstract class Chassis implements Executor{
 
     public Chassis(){
